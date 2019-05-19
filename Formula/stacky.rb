ad2a0ca20a01431c707ba00714fc8d1cb2c5b48e
mega.mac.slice <megamacslice@protonmail.com>
2019-05-19 01:13:03 +0000

    Update stacky.rb

--- Formula/stacky.rb
+++ Formula/stacky.rb
@@ -2,9 +2,9 @@ class Stacky < Formula
   include Language::Python::Virtualenv
   desc "Stacky"
   homepage "https://github.com/mega-mac-slice/stacky"
-  url "https://raw.githubusercontent.com/mega-mac-slice/stacky/master/dist/stacky-0.0.2.tar.gz"
-  sha256 "d8fad884229c26db592caa982937c850a4018a943766afeb79ddf18bebd3e7cc"
-  version "0.0.2"
+  url "https://raw.githubusercontent.com/mega-mac-slice/stacky/master/dist/stacky-0.0.3.tar.gz"
+  sha256 "3fcb84b2572f62dac0401ef2b7f2b9c5e5ed39f58b9ae808b18ed0578e681c27"
+  version "0.0.3"
   depends_on "python"
   depends_on "python3"
   def install
